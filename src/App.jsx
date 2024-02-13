168424ccc77ae9de96aa573548130bd2ddc8e4bd
workwithraymond <workwithraymond01@gmail.com>
2024-02-13 06:37:59 -0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Navbar from "./components/Navbar";
 
 function App() {
     return (
-    <main>
-      <Navbar/>
-    </main>
+  <main>
+    <Navbar />
+  </main>
 
-    );
+    )
   }
   export default App;
