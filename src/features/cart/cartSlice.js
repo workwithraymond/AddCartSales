70581b810a3fb37257d69d06c5efa4c95b446066
workwithraymond <workwithraymond01@gmail.com>
2024-02-16 06:06:33 -0500

    new commit

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,10 +14,35 @@ const cartSlice = createSlice({
     reducers: {
         clearCart:(state) => {
            state.cartItems = [] 
+        },
+        removeItems : (state, action) => {
+           const itemId = action.payload;
+           state.cartItems = state.cartItems.filter((item) => 
+           item.id !== itemId);
+        },
+        increase: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id
+            === payload.id)
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id
+            === payload.id)
+            cartItem.amount = cartItem.amount - 1;
+        },
+        calculateTotals : (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item) => {
+                amount += item.amount 
+                total += item.amount * item.price
+            })
+            state.amount = amount;
+            state.total = total;
         }
-    }
+    },
 });
 
-export const {clearCart} = cartSlice.actions;
+export const {clearCart, removeItem, increase, decrease, calculateTotals} = cartSlice.actions;
 
 export default cartSlice.reducer;
