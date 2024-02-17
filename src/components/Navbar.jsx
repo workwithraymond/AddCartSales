9b0f49c882018c1e694ba4fe765e623cf2ac6f6c
workwithraymond <workwithraymond01@gmail.com>
2024-02-16 21:42:04 -0500

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
   <nav>
         <div className='nav-center'>
-        <h3>redux toolkit</h3>
+        <h3>Cell Phone Kit</h3>
         <div className='nav-container'>
           
         <CartIcon/>
